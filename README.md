7cef30270fa1af46c53ea28417e475e6faa8c176
_ <_@_>
2022-01-21 21:39:42 +0800

    chore: init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# EDR-bypass
+
+Bypass SSO EDR check with fake response
+
+1. pull this repo
+2. put the edr-bypass.crt to your system as always trusted
+3. yarn
+4. yarn start
+5. enjoy!
---
+++ index.js
@@ -0,0 +1,45 @@
+const PORT = 19777;
+
+const express = require("express");
+const app = express();
+const https = require("https");
+const cors = require("cors");
+const fs = require("fs");
+const { networkInterfaces } = require("os");
+
+const nets = networkInterfaces();
+const flattenedNets = Object.values(nets).flat();
+const { address, mac } = flattenedNets.find(
+  ({ family, internal }) => family === "IPv4" && !internal
+);
+
+const server = https.createServer(
+  {
+    key: fs.readFileSync("./edr-bypass.private.key", "utf8"),
+    cert: fs.readFileSync("./edr-bypass.crt", "utf8"),
+  },
+  app
+);
+
+const fakeResponse = {
+  ret: 0,
+  data: {
+    dlp: true,
+    mac_address: mac,
+    device_id: "",
+    ip: address,
+    spend_time: 0,
+    is_install_soft: true,
+  },
+};
+
+app.use(cors());
+app.post("/", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(fakeResponse);
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}, with response:`);
+  console.log(fakeResponse);
+});
